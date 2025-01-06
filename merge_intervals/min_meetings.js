b8a7815b1d7576c80435540fe9d137c1690dff40
Rekha-B <baburekha002691@gmail.com>
2025-01-06 19:31:38 +0530

    Added min meetings

--- merge_intervals/min_meetings.js
+++ merge_intervals/min_meetings.js
@@ -5,5 +5,25 @@
 // Output: 2
 
 function minMeetingRooms(intervals){
-    
+    const startTimes = intervals.map(interval => interval[0]).sort((a,b) => a-b);
+    const endTimes = intervals.map(interval => interval[1]).sort((a,b) => a-b);
+
+    let startPointer = 0;
+    let endPointer = 0;
+    let rooms = 0;
+    let maxRooms = 0;
+    while(startPointer < intervals.length){
+        if(startTimes[startPointer] < endTimes[endPointer]){
+             rooms++;
+             startPointer++;
+             maxRooms = Math.max(maxRooms,rooms);
+        }
+        else {
+             rooms--;
+             endPointer++;
+        }
+    }
+    return maxRooms;
 }
+
+console.log(minMeetingRooms([[0,30],[5,10],[15,20]]));
