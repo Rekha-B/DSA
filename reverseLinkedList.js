73ee7a2c67cef46fb316d4b8d98caa56fc6ca775
Rekha-B <baburekha002691@gmail.com>
2026-04-06 19:07:09 +0530

    Create reverseLinkedList.js

---
+++ reverseLinkedList.js
@@ -0,0 +1,17 @@
+// Complexity
+// Time: O(n)
+// Space: O(1)
+
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+}
